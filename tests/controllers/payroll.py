0719c1c52bba16ff8f5228b244fed875022beea1
Swithez <59332696+Swithez@users.noreply.github.com>
2025-01-14 00:16:18 +0300

    Update payroll.py
    Создание комментариев для функций.

--- tests/controllers/payroll.py
+++ tests/controllers/payroll.py
@@ -3,24 +3,30 @@ import pandas as pd
 from flask_payroll.models.hourly_employee import HourlyEmployee  
 from flask_payroll.models.salaried_employee import SalariedEmployee
 
+# Класс для управления ведомостью заработной платы.
 class Payroll:
     def __init__(self):
         self.employees = []
 
+    # Добавляет сотрудника в ведомость.
     def add_employee(self, employee):
         self.employees.append(employee)
 
+    # Вычисляет общую сумму заработной платы всех сотрудников.
     def calculate_total_payroll(self):
         return sum(employee.calculate_salary() for employee in self.employees)
 
+    # Вычисляет среднюю заработную плату.
     def get_average_salary(self):
         if not self.employees:
             return 0
         return self.calculate_total_payroll() / len(self.employees)
 
+    # Возвращает список сотрудников заданного типа.
     def get_employees_by_type(self, employee_type):
         return [emp for emp in self.employees if isinstance(emp, employee_type)]
 
+    # Импортирует данные о сотрудниках из JSON-файла.
     def import_employees_from_json(self, file_path):
         try:
             with open(file_path, 'r', encoding="utf-8") as file:
@@ -41,6 +47,7 @@ class Payroll:
         except KeyError as e:
             print(f"Missing key {e} in employee data: {emp}")
 
+    # Вычисляет статистику по заработной плате.
     def get_statistics(self):
         salaries = [employee.calculate_salary() for employee in self.employees]
         if not salaries:  # Проверка на пустой список
