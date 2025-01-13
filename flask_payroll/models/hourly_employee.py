cd7083ea6bc92ce66ee4436cb7441087c25ef9ee
Swithez <59332696+Swithez@users.noreply.github.com>
2025-01-14 00:20:29 +0300

    Update hourly_employee.py
    Создание комментариев для функций.

--- flask_payroll/models/hourly_employee.py
+++ flask_payroll/models/hourly_employee.py
@@ -1,5 +1,6 @@
 from .employee import Employee
 
+# Класс для представления почасового сотрудника.
 class HourlyEmployee:
     def __init__(self, name, employee_id, hourly_rate, hours_worked):
         self.name = name
@@ -7,9 +8,11 @@ class HourlyEmployee:
         self.hourly_rate = hourly_rate
         self.hours_worked = hours_worked
 
+    # Рассчитывает заработную плату почасового сотрудника.
     def calculate_salary(self):
         return self.hourly_rate * self.hours_worked
 
+    # Преобразует данные почасового сотрудника в словарь.
     def to_dict(self):
         return {
             "name": self.name,
@@ -19,5 +22,6 @@ class HourlyEmployee:
             "type": "Почасовая оплата"
         }
 
+    #  Представляет объект HourlyEmployee в читаемом виде для удобства отладки.
     def __repr__(self):
         return f"HourlyEmployee(name='{self.name}', id={self.employee_id}, rate={self.hourly_rate}, hours={self.hours_worked})"
