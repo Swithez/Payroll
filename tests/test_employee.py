393fd13e21fab605f9fa0d39bb3dc9c4cedc2025
Swithez <59332696+Swithez@users.noreply.github.com>
2025-01-14 00:06:04 +0300

    Update test_employee.py
    Создание комментариев для функций.

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -3,18 +3,21 @@ from flask_payroll.models.hourly_employee import HourlyEmployee
 from flask_payroll.models.salaried_employee import SalariedEmployee
 from flask_payroll.models.employee import Employee
 
+# Тест создания HourlyEmployee
 def test_hourly_employee_creation():
     hourly_employee = HourlyEmployee("Иван", 1, 100, 20)
     assert hourly_employee.name == "Иван"
     assert hourly_employee.employee_id == 1
     assert hourly_employee.hourly_rate == 100
     assert hourly_employee.hours_worked == 20
-
+    
+# Тест расчета зарплаты HourlyEmployee
 def test_hourly_employee_calculate_salary():
     hourly_employee = HourlyEmployee("Иван", 1, 100, 20)
     salary = hourly_employee.calculate_salary()
     assert salary == 2000
-
+    
+# Тест преобразования HourlyEmployee в словарь
 def test_hourly_employee_to_dict():
     hourly_employee = HourlyEmployee("Иван", 1, 100, 20)
     employee_dict = hourly_employee.to_dict()
@@ -25,21 +28,26 @@ def test_hourly_employee_to_dict():
         "hours_worked": 20,
         "type": "Почасовая оплата"
     }
+
+# Тест строкового представления HourlyEmployee
 def test_hourly_employee_repr():
     hourly_employee = HourlyEmployee("Иван", 1, 100, 20)
     assert repr(hourly_employee) == "HourlyEmployee(name='Иван', id=1, rate=100, hours=20)"
 
+# Тест создания SalariedEmployee
 def test_salaried_employee_creation():
     salaried_employee = SalariedEmployee("Петр", 2, 50000)
     assert salaried_employee.name == "Петр"
     assert salaried_employee.employee_id == 2
     assert salaried_employee.annual_salary == 50000
-
+    
+# Тест расчета зарплаты SalariedEmployee
 def test_salaried_employee_calculate_salary():
     salaried_employee = SalariedEmployee("Петр", 2, 50000)
     salary = salaried_employee.calculate_salary()
     assert salary == 50000
 
+# Тест преобразования SalariedEmployee в словарь
 def test_salaried_employee_to_dict():
     salaried_employee = SalariedEmployee("Петр", 2, 50000)
     employee_dict = salaried_employee.to_dict()
@@ -50,11 +58,12 @@ def test_salaried_employee_to_dict():
         "type": "Контрактная оплата"
     }
 
+# Тест строкового представления SalariedEmployee
 def test_salaried_employee_repr():
     salaried_employee = SalariedEmployee("Петр", 2, 50000)
     assert repr(salaried_employee) == "SalariedEmployee(name='Петр', id=2, salary=50000)"
 
-
+# Проверка создания экземпляра абстрактного класса
 def test_employee_is_abstract():
     with pytest.raises(TypeError):
-        Employee("Test", 1) # Проверка, что нельзя создать экземпляр абстрактного класса
+        Employee("Test", 1)
